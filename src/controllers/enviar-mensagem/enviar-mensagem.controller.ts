4333089f1d256df8bacee8030c298e03f3ef5262
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2023-11-25 21:02:38 -0300

    enviar mensagem feito

--- src/controllers/enviar-mensagem/enviar-mensagem.controller.ts
+++ src/controllers/enviar-mensagem/enviar-mensagem.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { EnviarMensagemService } from 'src/services/enviar-mensagem/enviar-mensagem.service';
 
-@Controller('enviar-mensagem')
-export class EnviarMensagemController {}
+interface Data {
+  MEN_MENSAGEM: string;
+  FK_TOPICO_TOP_ID: number;
+  FK_USUARIOS_USR_ID: number;
+}
+
+@Controller('message')
+export class EnviarMensagemController {
+  constructor(private readonly enviarService: EnviarMensagemService) {}
+
+  @Post('/new')
+  async enviarMensagem(@Body() data: Data) {
+    return await this.enviarService.EnviarMensagem(data);
+  }
+}
--- src/services/enviar-mensagem/enviar-mensagem.service.ts
+++ src/services/enviar-mensagem/enviar-mensagem.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
+interface Data {
+  MEN_MENSAGEM: string;
+  FK_TOPICO_TOP_ID: number;
+  FK_USUARIOS_USR_ID: number;
+}
+
 @Injectable()
 export class EnviarMensagemService {
   constructor(private prisma: PrismaService) {}
 
-  async EnviarMensagem(data) {}
+  async EnviarMensagem(data: Data) {
+    try {
+      const mensagem = await this.prisma.mensagens.create({
+        data: {
+          MEN_MENSAGEM: data.MEN_MENSAGEM,
+          FK_TOPICO_TOP_ID: data.FK_TOPICO_TOP_ID,
+          FK_USUARIOS_USR_ID: data.FK_USUARIOS_USR_ID,
+        },
+      });
+
+      return mensagem;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
